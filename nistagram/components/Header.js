3ec77fd42dd0a711b3bca2b26c8ed707a4989474
pnkkumar123 <pankajkanwar420@gmail.com>
2024-01-27 13:22:56 +0530

    first commit added headed section

---
+++ nistagram/components/Header.js
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon,
+} from '@heroicons/react/outline';
+
+
+const Header = () => {
+  return (
+    <div>
+      <div className="flex justify-between max-w-6xl">
+        {/* left */}
+        <div className="relative hidden lg:inline-grid w-50 h-50 cursor-pointer=">
+          <div className="w-50 h-50">
+            <Image src='/OIG3.jpeg' width={50} height={50} alt="Picture of the author" />
+          </div>
+        </div>
+        <div className='relative w-10 h-10 flex-shrink-0 cursor-pointer'>
+          <Image src='/OIG3 (1).jpeg' width={30} height={30} alt="Picture of the author" />
+        </div>
+      {/* middle search */}
+      <div>
+        <div className='h-5 w-5 text-gray-500'>
+         <SearchIcon />
+        </div>
+        <input type="text" placeholder='Search' />
+      </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Header;
+
---
+++ nistagram/src/app/page.js
@@ -0,0 +1,16 @@
+import Header from "../../components/Header";
+
+
+export default function Home() {
+  return (
+    <>
+    <div className="">
+      <h1>This is the instagram 2.0 build</h1>
+        {/* header */}
+        <Header/>
+        {/* feed */}
+           {/* modal */}
+    </div>
+    </>
+  )
+}
